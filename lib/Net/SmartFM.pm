3390ada6e61fd2c3529c013063a454a3ae54c742
noblejasper <nobjas@gmail.com>
2009-09-15 21:30:06 +0900

    create package and test

--- lib/Net/SmartFM.pm
+++ lib/Net/SmartFM.pm
@@ -5,16 +5,81 @@ use strict;
 use Carp;
 use 5.008_001;
 
-use version; $VERSION = qv('0.0.3');
+use version; our $VERSION = qv('0.0.3');
 
-# Other recommended modules (uncomment to use):
-#  use IO::Prompt;
-#  use Perl6::Export;
-#  use Perl6::Slurp;
-#  use Perl6::Say;
+use XML::Parser;
+use WebService::Simple;
+
+sub new {
+    my ( $self, %args ) = @_;
+
+    Carp::croak "You must set api_key" unless($args{api_key});
+    return bless {
+        'smart_fm' => WebService::Simple->new(
+            base_url => 'http://api.smart.fm/',
+            param    => { api_key => $args{api_key}, },
+        ),
+    }, $self;
+}
+
+sub smart_fm {
+    my $self = shift;
+
+    $self->{smart_fm} = WebService::Simple->new(
+        base_url => 'http://api.smart.fm/',
+    ) unless ( $self->{smart_fm} );
+    return $self->{smart_fm};
+}
+
+my $KEYS = {
+    'user_profile' => { sprintf => 'users/%s', hash_key =>'user' },
+    'user_study_lists'  => { sprintf => 'users/%s/lists',          hash_key =>'channel' },
+    'user_study_items'  => { sprintf => 'users/%s/items',          hash_key =>'items'   },
+    'user_following'    => { sprintf => 'users/%s/friends',        hash_key =>'user'    },
+    'user_followers'    => { sprintf => 'users/%s/followers',      hash_key =>'user'    },
+    'recent_sentences'  => { sprintf => '/sentences',             bared    => 1        },
+
+    # these is no test m(_ _)m
+    'search lists'      => { sprintf => '/lists/matching/%s',     hash_key =>'channel' },
+    'search_items'      => { sprintf => '/items/matching/%s',     hash_key =>'items'   },
+    'items_in_list'     => { sprintf => '/lists/%s/items',        hash_key =>'channel' },
+    'search_sentences'  => { sprintf => '/sentences/matching/%s', bared    => 1        },
+    'sentences_in_list' => { sprintf => '/lists/%s/sentences',    bared    => 1        },
+    'recent_lists'      => { sprintf => '/lists',                 hash_key =>'channel' },
+    'recent_items'      => { sprintf => '/items',                 hash_key =>'items'   },
+    'sessions'          => { sprintf => '/sessions',              bared    => 1        },
+
+    # why... request to http://api.smart.fm/users/noblejasper/study_results?api_key=xxx failed
+    # 'user_study_results' => { sprintf => 'users/%s/study_results', hash_key =>'' },
+};
+
+sub get {
+    my ( $self, $key_name, $value ) = @_;
+    my $key = $KEYS->{$key_name};
+    $value ||= '';
+    Carp::croak 'Undefines key name is ' . $key_name unless( $key );
+    return $self->_get_bared( sprintf( $key->{sprintf}, $value ) )
+        if ( defined $key->{bared} && $key->{bared} );
+    return $self->_get(
+        sprintf( $key->{sprintf}, $value || '' ),
+        defined $key->{hash_key} ? $key->{hash_key} : '',
+    );
+}
+sub _get {
+    my ( $self, $path, $hash_key ) = @_;
+    my $data = $self->smart_fm->get( $path );
+
+    return $hash_key
+        ? $data->parse_response->{$hash_key}
+        : $data->parse_response;
+}
+
+sub _get_bared {
+    my ( $self, $path ) = @_;
+    return $self->smart_fm->get( $path );
+}
 
 
-# Module implementation here
 
 
 1; # Magic true value required at end of module
@@ -33,9 +98,10 @@ This document describes Net::SmartFM version 0.0.1
 =head1 SYNOPSIS
 
     use Net::SmartFM;
-    my $api = Net::SmartFM->new(
+    my $api = Net::SmartFM->new({
         api_key => 'd6bu49h84yj85z2mgnbh5t4j',
-    );
+    });
+    $api->execute_request();
 
 =for author to fill in:
     Brief code example(s) here showing commonest usage(s).
---
+++ t/01.use.t
@@ -0,0 +1,43 @@
+use Test::More tests => 12;
+
+use Net::SmartFM;
+use Data::Dumper;
+
+my $api = Net::SmartFM->new(
+    api_key => 'test',
+);
+isa_ok($api, 'Net::SmartFM');
+isa_ok($api->smart_fm, 'WebService::Simple');
+
+my $noblejasper = $api->get('user_profile', 'noblejasper');
+check_get_profile( $noblejasper, {
+    name     => 'noblejasper',
+    id       => 'noblejasper',
+    birthday => '1985-06-11',
+    gender   => 'male',
+    href     => 'http://smart.fm/users/noblejasper',
+});
+sub check_get_profile {
+    my ( $data, $rules ) = @_;
+    foreach ( keys %{ $rules } ) {
+        is($data->{$_}, $rules->{$_});
+    }
+}
+
+my $study_lists = $api->get('user_study_lists', 'noblejasper');
+is( $study_lists->{link}, 'http://smart.fm/users/noblejasper');
+
+my $study_items = $api->get('user_study_items', 'noblejasper');
+ok( $study_items->{item} );
+
+my $followings = $api->get('user_following', 'noblejasper');
+ok( $followings );
+
+my $followers = $api->get('user_followers', 'noblejasper');
+ok( $followers );
+
+my $recent_sentences = $api->get('recent_sentences');
+ok( $recent_sentences );
+
+# why... request to http://api.smart.fm/users/noblejasper/study_results?api_key=xxx failed
+#my $study_lists = $api->get_user_study_results('noblejasper');
